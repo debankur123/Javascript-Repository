c2d5a729b8829277d21354173faf47745bda4b01
debankur123 <debankurbhattacharjee784@gmail.com>
2022-07-11 10:02:18 +0530

    commit callback

---
+++ callback.js
@@ -0,0 +1,25 @@
+// Advance topic of Javascript is Callbacks. We will be using this with an example 
+
+/* Scenario 1
+1. Register
+2. Send welcome email
+3.Login
+4.user data
+5. Dispaly user data
+*/
+
+function register(){
+    console.log("Register end..");
+}
+function sendEmail(){
+    console.log("email end..");
+}
+function login(){
+    console.log("login end..");
+}
+function getuserData(){
+    console.log("get user data end..");
+}
+function displayUserData(){
+    console.log("dispaly user data end..");
+}
