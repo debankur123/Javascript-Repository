78bfc5b5f2954be62e760cffe332c375b8126c14
debankur123 <debankurbhattacharjee784@gmail.com>
2022-10-06 21:36:22 +0530

    Practice Questions

--- IfElse.js
+++ IfElse.js
@@ -4,5 +4,23 @@ function trueorFalse(isitTrue){
     }
     return "No , It is False";
 }
-
 console.log(trueorFalse(true));
+
+function testSize(num){
+    if(num<5){
+        return "tiny"
+    }
+    else if(num<10){
+        return "Small"
+    }
+    else if(num<15){
+        return "Medium"
+    }
+    else if(num<20){
+        return "Large"
+    }
+    else{
+        return "Huge"
+    }
+}
+console.log(testSize(5))
