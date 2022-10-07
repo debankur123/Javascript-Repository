c400bb64446dff79d0401689af7918332fb133e0
debankur123 <debankurbhattacharjee784@gmail.com>
2022-10-07 20:30:02 +0530

    update string

--- strings.js
+++ strings.js
@@ -5,3 +5,13 @@ function wordBlanks(myNoun,myAdjective,myVerb,myAdverb){
 }
 console.log(wordBlanks("Dog","big","ran","quickly"));
 console.log(wordBlanks("bike",'slow','flew','slowly'));
+
+const myVar = "mathematics";
+console.log(myVar.charAt(0)); // m
+console.log(myVar.indexOf("ati")); // 6
+console.log(myVar.lastIndexOf("ath"));
+console.log(myVar.slice(5,7)); // excludes the last character.
+console.log(myVar.toUpperCase()); 
+
+const myVar2 = "geeks for geeks";
+console.log(myVar2.lastIndexOf("geeks")); // 10 
